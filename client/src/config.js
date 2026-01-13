4ea95aaa127e5fe42edebacf1893ce355b9e6bd6
chinu234 <chirayulodha2004@gmail.com>
2026-01-13 16:41:59 +0530

    update
    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,3 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  BASE_URL = "http://localhost:4000/";
-}
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export { BASE_URL };
